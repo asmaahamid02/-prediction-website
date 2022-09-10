b333a2b7c342db460dd59dcfbcc9fa767cb9a3bd
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-10 12:49:02 +0300

    add fetchApi function to fetch data from api fill image header with a random image from the api

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -11,4 +11,24 @@ window.addEventListener('load', () => {
   const gender_api = 'https://api.genderize.io?name='
   const age_api = 'https://api.agify.io/?name='
   const nationality_api = 'https://api.nationalize.io/?name='
+
+  //function to fetch certain api and return back a Promise data
+  async function fetchApi(url, parameter) {
+    try {
+      //fetch api url --- await is used to wait for the Promise
+      let result = await fetch(url + parameter)
+      //parse promise data into JSON object
+      let data = await result.json()
+
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  //fill the header image with random dog image from api
+  fetchApi(dog_images_api, 1).then((data) => {
+    //fetching JSON object
+    image.src = data.message
+  })
 })
