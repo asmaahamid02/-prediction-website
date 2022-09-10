e3f59f9fe7c791e24b428be9f8463d9eed7fb1a6
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-10 11:53:03 +0300

    decalre variables in index.js file

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,14 @@
 /* JavaScript */
+window.addEventListener('load', () => {
+  const username_input = document.querySelector('#username').value
+  const image = document.querySelector('#dog-img')
+  const username = document.querySelector('#js-username')
+  const gender = document.querySelector('#gender')
+  const age = document.querySelector('#age')
+  const nationality = document.querySelector('#nationality')
+
+  const dog_images_api = 'https://dog.ceo/api/breeds/image/random/'
+  const gender_api = 'https://api.genderize.io?name='
+  const age_api = 'https://api.agify.io/?name='
+  const nationality_api = 'https://api.nationalize.io/?name='
+})
