43f796ee9e5490081f65bb00b2dfe347bc2d7f8d
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-11 22:41:28 +0300

    create signup.js file declare variable check validations upon submitting the form

---
+++ assets/javascript/signup.js
@@ -0,0 +1,33 @@
+/* Sign up */
+
+window.addEventListener('load', () => {
+  const username = document.querySelector('#username')
+  const email = document.querySelector('#email')
+  const password = document.querySelector('#password')
+  const passowrd_confirm = document.querySelector('#password-confirm')
+  const submit_btn = document.querySelector('#submit')
+  const signup = document.querySelector('#signup')
+
+  function isEmpty(str) {
+    return !str.trim().length
+  }
+
+  signup.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (
+      isEmpty(username.value) ||
+      isEmpty(email.value) ||
+      isEmpty(password.value) ||
+      isEmpty(passowrd_confirm.value)
+    ) {
+      return
+    }
+
+    if (password.value != passowrd_confirm.value) {
+      return
+    }
+
+    console.log('success')
+  })
+})
