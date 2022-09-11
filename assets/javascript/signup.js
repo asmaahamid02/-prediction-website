a2399abd2f9a86bd24bfdc12a7143eb1c1130392
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-12 00:25:20 +0300

    add error essage if the validation goes wrong in signup form

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -7,6 +7,8 @@ window.addEventListener('load', () => {
   const passowrd_confirm = document.querySelector('#password-confirm')
   const submit_btn = document.querySelector('#submit')
   const signup = document.querySelector('#signup')
+  const hidden_wrapper = document.querySelector('.hidden-wrapper')
+  const error_message = document.querySelector('.error-message')
 
   function isEmpty(str) {
     return !str.trim().length
@@ -14,20 +16,28 @@ window.addEventListener('load', () => {
 
   signup.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    let error = ''
     if (
       isEmpty(username.value) ||
       isEmpty(email.value) ||
       isEmpty(password.value) ||
       isEmpty(passowrd_confirm.value)
     ) {
-      return
+      error += 'fill out all the fields!'
     }
 
     if (password.value != passowrd_confirm.value) {
-      return
+      if (!isEmpty(error)) {
+        error += '/ '
+      }
+      error += 'passwords are not matching'
     }
 
+    if (!isEmpty(error)) {
+      error_message.textContent = error
+      hidden_wrapper.style.display = 'block'
+      return
+    }
     let user = {
       username: username.value,
       password: password.value,
